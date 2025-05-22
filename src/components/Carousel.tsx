0897f272e1f8158411fd7cf160b698bf10d4dc1e
Dereck Bélanger <debel152@ulaval.ca>
2025-05-22 18:28:39 -0400

    CAROUSEL IS FIXED FINALLY!
    OMG NOBODY TOUCH THIS CURSED CAROUSEL. I finally found a way to make it not look broken, the loop isn't perfect but it's decent

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -6,27 +6,43 @@ const images = [
   "/banner/avenirti.png",
   "/banner/ulaval.png",
 ];
+
+const IMAGE_WIDTH = 120; // px
+const IMAGE_HEIGHT = 80; // px
+const IMAGE_MARGIN = 32; // px (mr-8)
+const REPEAT_COUNT = 6; // Repeat images 6 times for a much longer loop
+const TOTAL_WIDTH = (IMAGE_WIDTH + IMAGE_MARGIN) * images.length * REPEAT_COUNT;
+
 export default function InfiniteScrollBanner() {
+  // Repeat the images array REPEAT_COUNT times
+  const repeatedImages = Array(REPEAT_COUNT)
+    .fill(images)
+    .flat();
+
   return (
     <div className="scroll-banner relative w-full bg-black py-4 overflow-hidden">
       <motion.div
         className="flex"
         animate={{
-          x: [0, `-${images.length * 272}px`],
+          x: [0, `-${TOTAL_WIDTH}px`],
         }}
         transition={{
-          duration: 13,
+          duration: 13 * REPEAT_COUNT, // Make the duration proportional to the repeat count
           ease: "linear",
           repeat: Infinity,
           repeatType: "loop",
         }}
       >
-        {[...images, ...images, ...images].map((src, index) => (
+        {repeatedImages.map((src, index) => (
           <img
             key={index}
             src={src}
             alt={`banner image ${(index % images.length) + 1}`}
-            className="h-44 w-52 object-cover mr-24 inline-block opacity-80"
+            className="h-20 w-30 object-contain mr-8 inline-block opacity-80"
+            style={{
+              width: `${IMAGE_WIDTH}px`,
+              height: `${IMAGE_HEIGHT}px`,
+            }}
           />
         ))}
       </motion.div>
