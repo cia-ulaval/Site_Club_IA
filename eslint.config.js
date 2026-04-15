d7a2ab8c720fc2c7c70ac18e896d4b10f8e9d672
Dereck Bélanger <debel152@ulaval.ca>
2026-04-15 19:12:55 -0400

    added linter and proper CI

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+
+export default [
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: { parser: tsParser },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off", // not needed in React 17+
+    },
+    settings: { react: { version: "detect" } },
+  },
+];
